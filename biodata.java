daef2865f1d86e80847f9f8b1abe713a8afd46fe
Nafis17 <sabitgt17@gmail.com>
2021-04-03 13:27:35 +0800

    filejava

---
+++ biodata.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public class biodata
+{
+ public static void main(String[] args){
+  BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+  String name = "";
+  String npm = "";
+  String alamat = "";
+  
+  try
+  {
+   System.out.print("Input Nama  : ");
+   name = dataIn.readLine();
+   System.out.print("Input NPM  : ");
+   npm = dataIn.readLine();
+   System.out.print("Input Alamat  : ");
+   alamat = dataIn.readLine();
+   System.out.println("");
+   System.out.println("");
+  }
+  catch( IOException e )
+  {
+   System.out.println("Error!");
+  }
+   
+  System.out.println("-------------------BIODATA----------------------");
+  System.out.println("Nama  : " + name);
+  System.out.println("NPM  : " + npm);
+  System.out.println("Alamat  : " + alamat);
+  System.out.println("------------------------------------------------");
+ }
+}
